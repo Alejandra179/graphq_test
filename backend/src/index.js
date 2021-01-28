8d2674b967e75553cd24f047ea266d6490039840
Alejandra179 <69905852+Alejandra179@users.noreply.github.com>
2021-01-28 17:59:36 -0300

    28-01

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,42 @@
-require('dotenv').config();
-const app = require('./server');
-require('./database');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const { createServer } = require('http')
+const { ApolloServer, PubSub } = require('apollo-server-express')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 
+const startServer = async () => {
+    const PORT = process.env.PORT || 4000
+    const ATLAS_URI = process.env.ATLAS_URI
+    const app = express()
+    const httpServer = createServer(app)
+    const pubsub = new PubSub()
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: ({ req }) => ({ req, pubsub })
+    })
+    
+    server.applyMiddleware({ app, path: '/graphql', cors: false })
+    server.installSubscriptionHandlers(httpServer)
 
-app.listen(app.get('port'), ()=>{
-    console.log('servidor corriendo en el puerto', app.get('port'));
-});
+    await mongoose.connect(ATLAS_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => {
+        console.log('Database connected');
+    })
+    .catch(err => {
+        console.log('ERROR: ' + err)
+        process.exit()
+    })
+
+    httpServer.listen(PORT, () => {
+        console.log(`Servidor corriendo en el puerto ${PORT}`)
+    })
+}
+
+startServer()
